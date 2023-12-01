13cffce987658aabd8b2aec34cc9d9e30b8a05a3
ivan.chalakov <ivan.chalakov@alpha-devs.com>
2023-12-01 11:59:21 +0200

    add flag to add option to change notification

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"time"
 
 	"github.com/gen2brain/beeep"
 )
 
 func main() {
+	mins := flag.Int("mins", 60, "awaiting minutes (int)")
+	flag.Parse()
+
 	for {
-		time.Sleep(60 * time.Minute)
+		fmt.Printf("Next notification will be after %d minutes.", *mins)
+		time.Sleep(time.Duration(*mins) * time.Minute)
 
 		err := beeep.Notify("Water Reminder", "Time to dring water!", "")
 		if err != nil {
